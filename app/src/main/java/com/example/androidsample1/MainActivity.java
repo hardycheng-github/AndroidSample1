14eec8b4c3e90c04b740ead547cab71cc7c0d63c
hardycheng <hardy40105@gmail.com>
2022-04-06 17:34:16 +0800

    service sample

--- app/src/main/java/com/example/androidsample1/MainActivity.java
+++ app/src/main/java/com/example/androidsample1/MainActivity.java
@@ -9,68 +9,110 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     //static string TAG for logcat debug
     private static final String TAG = MainActivity.class.getSimpleName();
     private SampleService mService;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            Log.d(TAG, "onServiceConnected");
+            try {
+                mService = ((SampleService.SampleBinder) iBinder).getService();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.d(TAG, "onServiceDisconnected");
+            mService = null;
+        }
+    };
+
+    private Button btnStartService, btnStopService, btnBindService, btnUnbindService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate");
-        bindSampleService();
+        Log.d(TAG, "Lifecycle.onCreate");
+        findView();
+        initView();
+    }
+
+    private void findView(){
+        //old compiler need class transform
+        btnStartService = findViewById(R.id.btn_start_service);
+        btnStopService = findViewById(R.id.btn_stop_service);
+        btnBindService = findViewById(R.id.btn_bind_service);
+        btnUnbindService = findViewById(R.id.btn_unbind_service);
+    }
+
+    private void initView(){
+        //without LAMBDA expression
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSampleService();
+            }
+        });
+        //create interface with LAMBDA expression
+        btnStopService.setOnClickListener(v-> stopSampleService());
+        btnBindService.setOnClickListener(v-> bindSampleService());
+        btnUnbindService.setOnClickListener(v-> unbindSampleService());
     }
 
     @Override
     public void onStart(){
         super.onStart();
-        Log.d(TAG, "onStart");
+        Log.d(TAG, "Lifecycle.onStart");
     }
 
     @Override
     public void onResume(){
         super.onResume();
-        Log.d(TAG, "onResume");
+        Log.d(TAG, "Lifecycle.onResume");
     }
 
     @Override
     public void onPause(){
         super.onPause();
-        Log.d(TAG, "onPause");
+        Log.d(TAG, "Lifecycle.onPause");
     }
 
     @Override
     public void onStop(){
         super.onStop();
-        Log.d(TAG, "onStop");
+        Log.d(TAG, "Lifecycle.onStop");
     }
 
     @Override
     public void onDestroy(){
         super.onDestroy();
-        Log.d(TAG, "onDestroy");
+        Log.d(TAG, "Lifecycle.onDestroy");
     }
 
     private void bindSampleService(){
         Intent intent = new Intent(this, SampleService.class);
-        bindService(intent, new ServiceConnection() {
-            @Override
-            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-                Log.d(TAG, "onServiceConnected");
-                try {
-                    mService = ((SampleService.SampleBinder) iBinder).getService();
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
 
-            @Override
-            public void onServiceDisconnected(ComponentName componentName) {
-                Log.d(TAG, "onServiceDisconnected");
-                mService = null;
-            }
-        }, Context.BIND_AUTO_CREATE);
+    private void unbindSampleService(){
+        unbindService(mConnection);
+    }
+
+    private void startSampleService(){
+        Intent intent = new Intent(this, SampleService.class);
+        startService(intent);
+    }
+
+    private void stopSampleService(){
+        Intent intent = new Intent(this, SampleService.class);
+        stopService(intent);
     }
 }
--- app/src/main/java/com/example/androidsample1/SampleService.java
+++ app/src/main/java/com/example/androidsample1/SampleService.java
@@ -1,16 +1,33 @@
 package com.example.androidsample1;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 public class SampleService extends Service {
     private static final String TAG = SampleService.class.getSimpleName();
 
     private long startTime;
     private boolean isRunning = false;
+    private Thread runtimeCounter = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG, "Runtime Counter Start!");
+            while(isRunning){
+                try {
+                    Log.i(TAG, String.format("Service already running: %d secs", getRuntimeSeconds()));
+                    runtimeCounter.sleep(1000);
+                } catch (InterruptedException e){
+                    Log.d(TAG, "Thread sleep interrupted!");
+                }
+            }
+            Log.d(TAG, "Runtime Counter Finish.");
+        }
+    });
 
     public class SampleBinder extends Binder {
         private SampleService mService;
@@ -24,34 +41,31 @@ public class SampleService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d(TAG, "onBind");
+        Log.d(TAG, "Lifecycle.onBind");
         return new SampleBinder(this);
     }
 
-    public void onCreate(){
-        super.onCreate();
-        Log.d(TAG, "onCreate");
-        isRunning = true;
-        startTime = System.currentTimeMillis();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(isRunning){
-                    try {
-                        Log.i(TAG, String.format("Service already running: %d secs", getRuntimeSeconds()));
-                        Thread.sleep(1000);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
+    @Override
+    public boolean onUnbind(Intent intent){
+        Log.d(TAG, "Lifecycle.onUnbind");
+        return super.onUnbind(intent);
     }
 
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.d(TAG, "Lifecycle.onCreate");
+        isRunning = true;
+        startTime = System.currentTimeMillis();
+        runtimeCounter.start();
+    }
+
+    @Override
     public void onDestroy(){
         super.onDestroy();
-        Log.d(TAG, "onDestroy");
+        Log.d(TAG, "Lifecycle.onDestroy");
         isRunning = false;
+        runtimeCounter.interrupt();
     }
 
     public int getRuntimeSeconds(){
