51904f36929e099a5f57c6b8a4c9e01640730f3f
hardycheng <hardy40105@gmail.com>
2022-04-18 11:35:16 +0800

    service buttons and lifecycle update

--- app/src/main/java/com/example/androidsample1/MainActivity.java
+++ app/src/main/java/com/example/androidsample1/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.androidsample1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.content.ComponentName;
 import android.content.Context;
@@ -11,30 +16,39 @@ import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //static string TAG for logcat debug
     private static final String TAG = MainActivity.class.getSimpleName();
+    private Context mContext = this;
     private SampleService mService;
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             Log.d(TAG, "onServiceConnected");
-            try {
-                mService = ((SampleService.SampleBinder) iBinder).getService();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+            mService = ((SampleService.SampleBinder) iBinder).getService();
         }
 
         @Override
         public void onServiceDisconnected(ComponentName componentName) {
+            //this section only when service crash
             Log.d(TAG, "onServiceDisconnected");
             mService = null;
         }
     };
 
-    private Button btnStartService, btnStopService, btnBindService, btnUnbindService;
+    private Button btnStartService, btnStopService, btnBindService, btnUnbindService, btnGetRuntime;
+
+    public MainActivity(){
+        super();
+        getLifecycle().addObserver(new LifecycleEventObserver() {
+            @Override
+            public void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event) {
+                Log.d(TAG, "onStateChanged: " + event.name());
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Lifecycle.onCreate");
         findView();
         initView();
+        //Lifecycle only use for AppCompatActivity, not for Activity
+
     }
 
     private void findView(){
@@ -51,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         btnStopService = findViewById(R.id.btn_stop_service);
         btnBindService = findViewById(R.id.btn_bind_service);
         btnUnbindService = findViewById(R.id.btn_unbind_service);
+        btnGetRuntime = findViewById(R.id.btn_get_runtime);
     }
 
     private void initView(){
@@ -58,61 +75,23 @@ public class MainActivity extends AppCompatActivity {
         btnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startSampleService();
+                Intent intent = new Intent(mContext, SampleService.class);
+                startService(intent);
             }
         });
         //create interface with LAMBDA expression
-        btnStopService.setOnClickListener(v-> stopSampleService());
-        btnBindService.setOnClickListener(v-> bindSampleService());
-        btnUnbindService.setOnClickListener(v-> unbindSampleService());
-    }
-
-    @Override
-    public void onStart(){
-        super.onStart();
-        Log.d(TAG, "Lifecycle.onStart");
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        Log.d(TAG, "Lifecycle.onResume");
-    }
-
-    @Override
-    public void onPause(){
-        super.onPause();
-        Log.d(TAG, "Lifecycle.onPause");
-    }
-
-    @Override
-    public void onStop(){
-        super.onStop();
-        Log.d(TAG, "Lifecycle.onStop");
-    }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG, "Lifecycle.onDestroy");
-    }
-
-    private void bindSampleService(){
-        Intent intent = new Intent(this, SampleService.class);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-    }
-
-    private void unbindSampleService(){
-        unbindService(mConnection);
-    }
-
-    private void startSampleService(){
-        Intent intent = new Intent(this, SampleService.class);
-        startService(intent);
-    }
-
-    private void stopSampleService(){
-        Intent intent = new Intent(this, SampleService.class);
-        stopService(intent);
+        btnStopService.setOnClickListener(v-> {
+            Intent intent = new Intent(mContext, SampleService.class);
+            stopService(intent);
+        });
+        btnBindService.setOnClickListener(v-> {
+            Intent intent = new Intent(this, SampleService.class);
+            bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        });
+        btnUnbindService.setOnClickListener(v-> unbindService(mConnection));
+        btnGetRuntime.setOnClickListener(v-> {
+            Toast.makeText(this, String.format("runtime: %ds", mService.getRuntimeSeconds())
+                    , Toast.LENGTH_SHORT).show();
+        });
     }
 }
--- app/src/main/java/com/example/androidsample1/SampleService.java
+++ app/src/main/java/com/example/androidsample1/SampleService.java
@@ -1,23 +1,26 @@
 package com.example.androidsample1;
 
-import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
-public class SampleService extends Service {
+import androidx.annotation.NonNull;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleEventObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LifecycleService;
+
+// LifecycleService dependency: 'androidx.lifecycle:lifecycle-extensions'
+public class SampleService extends LifecycleService {
     private static final String TAG = SampleService.class.getSimpleName();
 
     private long startTime;
-    private boolean isRunning = false;
     private Thread runtimeCounter = new Thread(new Runnable() {
         @Override
         public void run() {
             Log.d(TAG, "Runtime Counter Start!");
-            while(isRunning){
+            while(getLifecycle().getCurrentState().isAtLeast(Lifecycle.State.STARTED)){
                 try {
                     Log.i(TAG, String.format("Service already running: %d secs", getRuntimeSeconds()));
                     runtimeCounter.sleep(1000);
@@ -39,33 +42,27 @@ public class SampleService extends Service {
         }
     }
 
+    public SampleService(){
+        super();
+        getLifecycle().addObserver(new LifecycleEventObserver() {
+            @Override
+            public void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event) {
+                Log.d(TAG, "onStateChanged: " + event.name());
+                if(event.equals(Lifecycle.Event.ON_START)){
+                    startTime = System.currentTimeMillis();
+                    runtimeCounter.start();
+                } else if(event.equals(Lifecycle.Event.ON_STOP)){
+                    startTime = 0;
+                    runtimeCounter.interrupt();
+                }
+            }
+        });
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
-        Log.d(TAG, "Lifecycle.onBind");
+        super.onBind(intent); //always return null
         return new SampleBinder(this);
-    }
-
-    @Override
-    public boolean onUnbind(Intent intent){
-        Log.d(TAG, "Lifecycle.onUnbind");
-        return super.onUnbind(intent);
-    }
-
-    @Override
-    public void onCreate(){
-        super.onCreate();
-        Log.d(TAG, "Lifecycle.onCreate");
-        isRunning = true;
-        startTime = System.currentTimeMillis();
-        runtimeCounter.start();
-    }
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        Log.d(TAG, "Lifecycle.onDestroy");
-        isRunning = false;
-        runtimeCounter.interrupt();
     }
 
     public int getRuntimeSeconds(){
